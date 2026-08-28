33fa6c264bb5e0d613c3939867332957151c2e85
Sara1502 <sara.moreira1520@gmail.com>
2026-08-28 13:58:32 -0300

    Ajuste de dificuldade

--- colossus/colossus.lua
+++ colossus/colossus.lua
@@ -3,10 +3,10 @@ local colossus = {}
 colossus.name = 'Prismarine Colossus'
 colossus.description = 'Enorme estátua de prismarine que mede mais de 20 metros de altura.'
 
-colossus.maxHealth = 50
-colossus.health = 50
+colossus.maxHealth = 100
+colossus.health = 100
 colossus.attack = 5
 colossus.defense = 2
-colossus.speed = 0
+colossus.speed = 1
 
 return colossus
--- player/player.lua
+++ player/player.lua
@@ -10,5 +10,4 @@ player.defense = 2
 player.speed = 3
 
 player.potions = 3
-
 return player
